e632eb78da8164b128fe6161f8579d892fe28aa3
LAPTOP-HVT5UM59\haral <harald.b.jordalen@gmail.com>
2023-05-31 13:48:33 +0200

    Implementation of BV-info.txt file, summarizing simulation and calculated parameters

--- main_simulation.m
+++ main_simulation.m
@@ -8,50 +8,56 @@ fprintf(fid, 'BV related information from last simulation\n');
 
 
 %%%%%%%%%%% Flags
-searchPresetVolume = true;  % search for preset volume, false use param.h_bot 
-useCTDprofile = true;      % boolean variable, (true/false) idicate wheter ctd profile is available.  
-fprintf(fid, 'Flags: \nsearchPresetVolume: %d \nuseCTDprofile: %d\n\n', searchPresetVolume, useCTDprofile);  
+flag.searchPresetVolume = true;  % search for preset volume, false use param.h_bot 
+flag.useCTDprofile = true;       % boolean variable, (true/false) idicate wheter ctd profile is available, false use constant density of fresh water. 
+flag.useSafetyRig = false;         % boolean variable, indicate wheter holder for safety line beeing used.
+flag.n_safetyRig = 0;             % number of holder for safety line (1 or 2)
+flag.fishTag = true;               % boolean variable, indicate wheter fish tag holder is monted.
+fprintf(fid, 'Flags: \nsearchPresetVolume: %d \nuseCTDprofile: %d\n', flag.searchPresetVolume, flag.useCTDprofile);
+fprintf(fid, 'useSafetyRig: %d \t number of safety holders %d\nuse fish tag holder: %d\n\n', flag.useSafetyRig, flag.n_safetyRig, flag.fishTag);
 %%%%%%%%%%
-CTD = load("CTD-probe/example_boorsa.mat"); % load CTD probe measurement (if not available , load example to avoid conflict in simulink)
+CTD = load("CTD-probe/borsa14.04.2023.mat"); % load CTD probe measurement (if not available , load "CTD-probe/example_boorsa.mat" to avoid conflict in simulink)
+%CTD = load("CTD-probe/example_borsa.mat");
 depth_CTD = CTD.Depth;
-%densityProfile = CTD.Density;
+maxDepth  = 10;
 
-if useCTDprofile % Load and reduce densityProfile array to 50 meters (target depth)
+
+if flag.useCTDprofile % Load and reduce densityProfile array to 50 meters (target depth)
     
-    index = interp1(depth_CTD, 1:length(depth_CTD), 50, 'nearest'); % find the nearest index for 50 meter depth 
+    index = interp1(depth_CTD, 1:length(depth_CTD), maxDepth, 'nearest'); % find the nearest index for 50 meter depth 
     depth_CTD = depth_CTD(1:index);
     densityProfile = CTD.Density(1:index);
 
-    rho_water_interval = max(densityProfile) - min(densityProfile)
+    rho_water_interval = max(densityProfile) - min(densityProfile);
 else % use constant water density
-    densityProfile = 999;    % constant water density (useCTDprofile = false)
+    densityProfile = 999;    % constant water density (flag.useCTDprofile = false)
     fprintf(fid, 'constant water density beeing used: %.1f [kg/m^3]\n', densityProfile);
 end
 % load dimention parameters in struct param.
 parameters
 g = 9.81; % [m/s^2]
 Cd_top = 1.28;%0.9 % Drag coefficient when floating to surface
-Cd_bottom  = 1; %Drag coefficient when sinking
+Cd_bottom  = 1.28; %Drag coefficient when sinking
 
 
-[V_max, V_piston] = calc_preset_volume(param);
-rho_min_vehicle = param.mass/V_max;
-if searchPresetVolume
-    param.h_bot = 0.08; %Initial value of outer lid height from vehicle bottom
-    if useCTDprofile
-        param.h_bot = search_presett_volume(param, min(densityProfile));
+% [V_max, V_piston] = calc_preset_volume(flag, param);
+% rho_min_vehicle = param.mass/V_max;
+if flag.searchPresetVolume
+    param.h_bot = 0.050; %Initial guess of outer lid height from vehicle bottom
+    if flag.useCTDprofile
+        param.h_bot = search_presett_volume(flag, param, min(densityProfile));
     else
-        param.h_bot = search_presett_volume(param, densityProfile);
+        param.h_bot = search_presett_volume(flag, param, densityProfile);
     end
 end
 fprintf(fid, 'Vehicle presetting of outer lid height from bottom: h_bot = %.1f [mm]\n', param.h_bot*1000);
 
-[V_max, V_piston] = calc_preset_volume(param);
+[V_max, V_piston] = calc_preset_volume(flag, param);
 rho_min_vehicle = param.mass/V_max;
 rho_max_vehicle = param.mass/(V_max-V_piston);
 delta_rho_vehicle = rho_max_vehicle - rho_min_vehicle;
 delta_rho_water = max(densityProfile) - min(densityProfile);
-h_pis_eq = find_piston_equilibrium(param, min(densityProfile));
+h_pis_eq = find_piston_equilibrium(flag, param, min(densityProfile));
 fprintf(fid, 'Piston position that result in equilibrium water surface %.2f [mm] \n\t piston_pos < %.2f --> floating \n\t piston_pos > %.2f --> sinking\n', h_pis_eq, h_pis_eq, h_pis_eq);
 fprintf(fid, 'h_bot seems to have an deviation of about 1.5mm (sim %.2f == real %.2f) BUT THIS NEDS VERIFICATION, AND MIGHT BE CHANGING WITH NEW BATTERY PACKAGE\n', param.h_bot*1000, (param.h_bot*1000-1.5));
 fprintf(fid, 'rho min vehicle: %.2f \t rho max vehicle %.2f \t delta rho vehicle: %.2f \n', rho_min_vehicle, rho_max_vehicle, delta_rho_vehicle);
@@ -60,9 +66,12 @@ if (min(densityProfile) ~= densityProfile(1))
    fprintf(fid, 'Warning! minimum water density not at surface. Make sure vehicle are able to float to surface.\n');
 end
 if rho_max_vehicle < max(densityProfile)
-    index = interp1(densityProfile, 1:length(densityProfile), rho_max_vehicle, 'nearest'); % find the nearest index for rho_max_density
-    max_diving_depth = depth_CTD(index);
-    fprintf(fid, 'Vehicle are unable to dive deeper then: %.1f meter\n',max_diving_depth);
+     if flag.useCTDprofile
+        index = interp1(densityProfile, 1:length(densityProfile), rho_max_vehicle, 'nearest'); % find the nearest index for rho_max_density
+        max_diving_depth = depth_CTD(index);
+        fprintf(fid, 'Vehicle are unable to dive deeper then: %.1f meter\n',max_diving_depth);
+
+     end
 end
 delta_pos_positon_min = (1/51200000); % Linear movement per step
 
@@ -75,53 +84,48 @@ fprintf(fid, '\nmaximum linear speed of piston: %.3f [mm/s]\n', v_piston_max*100
 %% plot density profile and vehicle density range.
 figure(1)
 hold on 
-if useCTDprofile
+if flag.useCTDprofile
     plot(densityProfile, depth_CTD, 'b');
 else
-   plot(densityProfile*[1,1], [1,50]) 
+   plot(densityProfile*[1,1], [0,maxDepth],'b'); 
 end
-plot(rho_min_vehicle*[1,1], [0, 50], 'r');
-plot(rho_max_vehicle*[1,1], [0, 50], 'y');
+plot(rho_min_vehicle*[1,1], [0, maxDepth], 'r');
+plot(rho_max_vehicle*[1,1], [0, maxDepth], 'y');
 hold off
 set(gca,'YDir','Reverse');
 grid();
 title('Density Profile');
 xlabel("Density [kg/m^3]");
 ylabel("Depth[m]");
-legend("water density profile", "min density vehicle", "max density cehicle");
+legend("water density profile", "min density vehicle", "max density vehicle");
 
 %% PID constants
 
-% tryout
+% optimum PID constant 
 Kp = 0.026;
 Ki = 0.001;
 Kd = 0.1;
-
-% Optimum fresh water tank parameters.
-% Kp = 0.026;
-% Ki = 0.001;
-% Kd = 0.1;
 
 alpha = 0.4; % Tuning parameter for EMA filter [0,1]
 sampleTime = 0.5;
 % sysd = tf([alpha, 0], [1, -(1-alpha)], sampleTime); %Discrete transfer function (EMA filter) 
 % sysc = d2c(sysd); % Transfer function for Pressure sensor (EMA filter)
 %bode(sysc) % Plot bode diagram of EMA filter
-integralTreshold = 2; %threshold in meter when integral is activated.
+integralTreshold = 10; %threshold in meter when integral is activated.
 offsetSensor = 0;
 
 
 % simulation depth and time
-step_depth1 = 10;
-step_depth2 = 5;
-stepTime = 180;
+step_depth1 = 5;
+step_depth2 = 0;
+stepTime = 120;
 
 fprintf(fid, '\nSimulation Parameters:\n');
 fprintf(fid, 'Kp = %.4f \nKi = %.4f \nKd = %.4f \nIntegral treshold = %.2f\n', Kp, Ki, Kd, integralTreshold);
 fprintf(fid, 'alpha (EMA filter) = %.2f \ntime constant = %.2f [seconds]\n', alpha, sampleTime); 
 
 %% Run Simulation 
-tspan = [0 180*2]; % Time span for simulation
+tspan = [0 60*2]; % Time span for simulation
 %options = simset('MaxStep', 0.5,'MinStep',1e-11, 'AbsTol', 1e-11, 'RelTol', 1e-11);
 %set_param('buoyancy2','AlgebraicLoopSolver','LineSearch');
 out = sim('buoyancy_simulation.slx',tspan);     %,options);
@@ -165,7 +169,7 @@ subplot(2,1,2);
     grid();
     hold off;
 
-fig6 = figure(6) % PID term contribution
+fig6 = figure(6); % PID term contribution
     hold on 
     plot(out.PIDterms.time, out.PIDterms.signals.values(:,1)*1000);
     plot(out.PIDterms.time, out.PIDterms.signals.values(:,2)*1000);
--- parameters.m
+++ parameters.m
@@ -12,8 +12,8 @@ param.r_house = 0.065; %[m]
 % h_bot is adjustable legth between wehicle house bottom, and outer lid. range = [0.010, 0.135]m (fresh water 0.0972 is found to be a god value)
 param.h_bot = 0.0972; % good value for fresh water tank (rho = 999)    
 % param.h_bot seems to have an deviation of about 1.5mm (sim 96.5 == real 95mm)
-param.h_bot = 0.089; %experimental for sea water
-
+%param.h_bot = 0.089; %experimental for sea water
+%param.h_bot = 0.0924; % experimental with new safety rig 
 
 param.Areal_vehicle_bottom = pi*param.r_house^2;
 param.Areal_vehicle_top = pi*param.r_outerLid^2;
@@ -30,9 +30,20 @@ param.Areal_piston = pi*param.r_piston^2; %piston areal
 % param.mass = 6.225;  %NEW MEASURMENT 3 + 1.54 + 1.75 + 0.07; %[kg] measured mass (6.36kg)
 % param.mass = 6.300; %[kg]measured 02.03.2023 at kybernetisk værksted 
 % stepper motor
-param.mass = 0.075 + 4.4470 + 1.746; % measured 13.03.20023 at kybernetisk verksted 
+param.mass = 0.075 + 4.4470 + 1.746; % [6.268 kg] measured 13.03.20023 at kybernetisk verksted led + house + outer lid 
 
-% tryput mass
-%param.mass = 6.223;
+if flag.useSafetyRig
+    param.safetyRig_mass =  (0.196); % Rig and screw/nut
+    param.mass = param.mass + param.safetyRig_mass*flag.n_safetyRig;
+    
+    param.safetyRig_volume = (110987e-9 + (0.030*pi*0.008^2 + 0.008*pi*0.011^2*2) ) * flag.n_safetyRig; %volume of rig (found with solidwork) + volume of bolt and nut;
+end
 
+if flag.fishTag
+    param.tagHolder_mass = 0.022; % measured mass of fish tag holder + tag
+    param.mass = param.mass + param.tagHolder_mass;
+    param.tagHolder_volume = 36273.00e-9; % volume of fish tag holder (including tag) found with solidwork
+end
+
+%param.mass = 6.492; 
 %other stuff...?
